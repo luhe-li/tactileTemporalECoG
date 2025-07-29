ee43f8a294c0f2aae1bb82cb9284a5dee5479b86
luhe-li <ll3981@nyu.edu>
2025-07-29 19:38:21 -0400

    extract data

--- temp_extract_data.m
+++ temp_extract_data.m
@@ -9,4 +9,11 @@ task              = 'temporalpattern';
 
 %% extract all session data
 
+% data: channel x time series across 5 runs
 [data, channels, events, srate] = bidsEcogGetPreprocData(dataPath, subject, [], task);
+
+% epoch by onset: time series x trial x channel
+align_onset_epoch_t = [-1, 1.5];  % stimulus epoch window
+[epochs, t] = ecog_makeEpochs(data, events.onset, align_onset_epoch_t, channels.sampling_frequency(1));
+
+save('ny726_data','data','epochs','t','channels','events','srate','subject')
