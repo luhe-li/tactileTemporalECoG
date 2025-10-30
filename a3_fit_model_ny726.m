e15569701640e8b95b848c21c5700f7d691aced8
luhe-li <114192631+luhe-li@users.noreply.github.com>
2025-10-30 13:24:34 -0400

    fit selected electrodes

--- a3_fit_model_ny726.m
+++ a3_fit_model_ny726.m
@@ -1,8 +1,8 @@
 
 clear;
 tbUse tactileTemporalECoG
-
 addpath(genpath(pwd, 'codes'))
+
 compute     = false;
 bidsDir     = tt_bidsRootPath;
 subject     = 'ny726';
@@ -19,7 +19,7 @@ specs.plot_data    = false;
 specs.plot_smooth  = 1; % could try some other values
 specs.epoch_t      = [-0.4, 1.8]; % stimulus epoch window
 specs.base_t       = [-0.4 -0.1]; % blank epoch window
-specs.chan_names   = {'C03', 'C04', 'C11', 'C12'};
+specs.chan_names   = {'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'W4','W5','W6','W9','W10','W11','W12','P3','P5','Z9','Z10','Z13','Z14','Z15','S9','S10'};
 specs.stim_names   = {'ONE-PULSE-1', 'ONE-PULSE-2', 'ONE-PULSE-3', 'ONE-PULSE-4', 'ONE-PULSE-5', 'ONE-PULSE-6',...
     'TWO-PULSE-1', 'TWO-PULSE-2', 'TWO-PULSE-3', 'TWO-PULSE-4', 'TWO-PULSE-5', 'TWO-PULSE-6'};
 [data, channel, t, srate]   = tt_prepareData(bidsDir, subject, session, task, numruns, inputFolder, description, specs);
@@ -38,19 +38,19 @@ options.xvalmode  = 0;      % 0 = none, 1 = stimulus leave-one-out
 options.display   = 'iter';  % 'iter' 'final' 'off'
 options.algorithm = 'bads';
 options.average_elecs = true;
+options.fitaverage = true;
 options.nfits     = 100; % if fit average
 
 % Compute model fit(s); data and fits will be saved to 'derivative/modelFit/results' folder
-tt_doModelFits(modelfun, stim_ts, s1_data, channel, srate, t, stim_info, options, [], 's1');
-tt_doModelFits(modelfun, stim_ts, m1_data, channel, srate, t, stim_info, options, [], 'm1');
+tt_doModelFits(modelfun, stim_ts, data, channel, srate, t, stim_info, options);
 
 %% 3. Evaluate model fits
 
 xvalmode = 1;
 datatype = 'electrodeaverages';
 
-[D{1}] = tt_loadDataForFigure(modelfun, xvalmode, datatype, [], 's1');
-[D{2}] = tt_loadDataForFigure(modelfun, xvalmode, datatype, [], 'm1');
+[D] = tt_loadDataForFigure(modelfun, xvalmode, datatype);
 
 saveDir = fullfile(bidsDir, 'derivatives', 'modelFit', 'figure', subject);
-tt_plotTactileParams(D, modelfun, saveDir);
+tt_plotDataAndFits(results, D.data, D.channels, D.stim, D.stim_info, D.t, D.options, saveDir, {'ONEPULSE', 'TWOPULSE'})
+tt_plotTactileVisualParams(D, modelfun, saveDir);
