d8106bdfe7bb4f566400a401ded92b94b78edbbf
luhe-li <114192631+luhe-li@users.noreply.github.com>
2025-11-03 18:17:57 -0500

    removed some electrodes, can we regenerate figure?

--- fit_group_average.m
+++ fit_group_average.m
@@ -1,6 +1,6 @@
 
 clear;
-tbUse tactileTemporalECoG
+% tbUse tactileTemporalECoG
 
 compute     = false;
 bidsDir     = tt_bidsRootPath;
@@ -20,7 +20,7 @@ specs.plot_data    = false;
 specs.plot_smooth  = 1; % could try some other values
 specs.epoch_t      = [-0.4, 1.8]; % stimulus epoch window
 specs.base_t       = [-0.4 -0.1]; % blank epoch window
-specs.chan_names   = {'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'W4','W5','W6','W9','W10','W11','W12','P3','P5','Z9','Z10','Z13','Z14','Z15','S9','S10'};
+specs.chan_names   = {'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'W4','W5','W6','W9','W10','W11','W12','P3','P5','Z13','Z14','Z15','S9','S10'};
 specs.stim_names   = {'ONE-PULSE-1', 'ONE-PULSE-2', 'ONE-PULSE-3', 'ONE-PULSE-4', 'ONE-PULSE-5', 'ONE-PULSE-6',...
     'TWO-PULSE-1', 'TWO-PULSE-2', 'TWO-PULSE-3', 'TWO-PULSE-4', 'TWO-PULSE-5', 'TWO-PULSE-6'};
 nyu_stim_names     = specs.stim_names;
@@ -59,11 +59,11 @@ channel = vertcat(channel1, channel2);
 % Generate stimulus timecourses, using the stimulus info file of nyu patients
 [stim_ts, stim_info] = tt_generateStimulusTimecourses(nyu_stim_names, t);
 
-modelfun = @LINEAR; % or @DN
+modelfun = {@DN, @LINEAR}; 
 
 % Define parameters
 options.doplots   = true;
-options.xvalmode  = 1;      % 0 = none, 1 = stimulus leave-one-out
+options.xvalmode  = 0;      % 0 = none, 1 = stimulus leave-one-out
 options.display   = 'iter';  % 'iter' 'final' 'off'
 options.algorithm = 'bads';
 options.average_elecs = true;
@@ -72,15 +72,15 @@ options.fitaverage = true;
 % Compute model fit(s); data and fits will be saved to 'derivative/modelFit/results' folder
 tt_doModelFits(modelfun, stim_ts, data, channel, srate, t, stim_info, options, [], 'group_average');
 
-%% 4. Model evaluation
-
-% Load data and fits
-modelfun = @DN;
-xvalmode = 1;
-datatype = 'electrodeaverages';
-[D] = tt_loadDataForFigure(modelfun, xvalmode, datatype);
-
-% Compute R2 and derived parameters
-objFunction = modelfun;
-includeDerivedParams = false;
-[results] = tt_evaluateModelFit(D,includeDerivedParams);
+% %% 4. Model evaluation
+% 
+% % Load data and fits
+% modelfun = @DN;
+% xvalmode = 1;
+% datatype = 'electrodeaverages';
+% [D] = tt_loadDataForFigure(modelfun, xvalmode, datatype);
+% 
+% % Compute R2 and derived parameters
+% objFunction = modelfun;
+% includeDerivedParams = false;
+% [results] = tt_evaluateModelFit(D,includeDerivedParams);
