87a3b5e4130e7a4da512db719ddd62e4bf28c03f
luhe-li <114192631+luhe-li@users.noreply.github.com>
2025-11-04 20:22:57 -0500

    zeropad overlap if next trial starts before the current epoch end

--- a3_fit_model_ny726.m
+++ a3_fit_model_ny726.m
@@ -1,6 +1,6 @@
 
 clear;
-tbUse tactileTemporalECoG
+% tbUse tactileTemporalECoG
 
 compute     = false;
 bidsDir     = tt_bidsRootPath;
@@ -18,7 +18,7 @@ specs.plot_data    = false;
 specs.plot_smooth  = 1; % could try some other values
 specs.epoch_t      = [-0.4, 1.8]; % stimulus epoch window
 specs.base_t       = [-0.4 -0.1]; % blank epoch window
-specs.chan_names   = {'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'W4','W5','W6','W9','W10','W11','W12','P3','P5','Z9','Z10','Z13','Z14','Z15','S9','S10'};
+specs.chan_names   = {'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'W4','W5','W6','W9','W10','W11','W12','P3','P5','Z13','Z14','Z15','S9','S10'};
 specs.stim_names   = {'ONE-PULSE-1', 'ONE-PULSE-2', 'ONE-PULSE-3', 'ONE-PULSE-4', 'ONE-PULSE-5', 'ONE-PULSE-6',...
     'TWO-PULSE-1', 'TWO-PULSE-2', 'TWO-PULSE-3', 'TWO-PULSE-4', 'TWO-PULSE-5', 'TWO-PULSE-6'};
 [data, channel, t, srate]   = tt_prepareData(bidsDir, subject, session, task, numruns, inputFolder, description, specs);
@@ -30,11 +30,14 @@ specs.stim_names   = {'ONE-PULSE-1', 'ONE-PULSE-2', 'ONE-PULSE-3', 'ONE-PULSE-4'
 
 modelfun          = {@DN,@LINEAR};
 
+trial_avg = mean(data,2);
+figure; hold on; plot(trial_avg(:,1));
+
 % Define parameters
 options.doplots   = false;
 options.xvalmode  = 0;      % 0 = none, 1 = stimulus leave-one-out
-options.display   = 'iter';  % 'iter' 'final' 'off'
-options.algorithm = 'fmincon';
+options.display   = 'off';  % 'iter' 'final' 'off'
+options.algorithm = 'bads';
 options.average_elecs = true;
 
 % Compute model fit(s); data and fits will be saved to 'derivative/modelFit/results' folder
